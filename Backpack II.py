8a1c03b7f23963f11a726ed70a08af447c644299
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-12-09 17:43:40 +0800

    f[i][v]=max{f[i-1][v],f[i-1][v-c[i]]+w[i]}

---
+++ Backpack II.py
@@ -0,0 +1,30 @@
+"""
+Given n items with size A[i] and value V[i], and a backpack with size m. What's the maximum value can you put into the
+backpack?
+"""
+__author__ = 'Danyang'
+class Solution:
+    def backPackII(self, m, A, V):
+        """
+        dp
+        f[i][v]=max{f[i-1][v],f[i-1][v-c[i]]+w[i]}
+        to
+        f[v]=max{f[v],f[v-c[i]+w[i]}
+
+        NEED TO KEEP A COPY OF (i-1) STATE.
+
+        :param m: An integer m denotes the size of a backpack
+        :param A & V: Given n items with size A[i] and value V[i]
+        :return: The maximum size
+        """
+        n = len(A)
+        f = [0 for _ in xrange(m+1)]  # plus 1 for dummy
+        for i in xrange(1, n+1):
+            copy = list(f)
+            for j in xrange(1, m+1):
+                # decide whether to put A[i-1]
+                if j-A[i-1]>=0:
+                    f[j] = max(copy[j], copy[j-A[i-1]]+V[i-1])
+                else:
+                    f[j] = copy[j]
+        return f[m]
---
+++ Binary Search.py
@@ -0,0 +1,31 @@
+"""
+Binary search is a famous question in algorithm.
+
+For a given sorted array (ascending order) and a target number, find the first index of this number in O(log n) time
+complexity.
+
+If the target number does not exist in the array, return -1.
+"""
+__author__ = 'Danyang'
+class Solution:
+    def binarySearch(self, nums, target):
+        """
+        basics
+
+        :param nums: The integer array
+        :param target: Target number to find
+        :return the first position of target in nums, position start from 0
+        """
+        l = 0
+        h = len(nums)
+        while l<h:
+            mid = (l+h)/2
+            if nums[mid]==target:
+                while mid>=0 and nums[mid-1]==nums[mid]: mid -= 1
+                return mid
+            elif nums[mid]<target:
+                l = mid+1
+            else:
+                h = mid
+        return -1
+
