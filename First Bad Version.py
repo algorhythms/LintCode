cb7516f99ad5ecbc78fa84fec16793eafbda18b9
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-12-16 23:13:26 +0800

    binary search, lower/upper bound

---
+++ First Bad Version.py
@@ -0,0 +1,29 @@
+"""
+The code base version is an integer and start from 1 to n. One day, someone commit a bad version in the code case, so
+it caused itself and the following versions are all failed in the unit tests.
+"""
+__author__ = 'Danyang'
+class VersionControl:
+    @classmethod
+    def isBadVersion(cls, id):
+        return True
+
+class Solution:
+    def findFirstBadVersion(self, n):
+        """
+
+        :param n: An integers.
+        :return: An integer which is the first bad version.
+        """
+        l = 1
+        h = n
+        while l<=h:
+            m = (l+h)/2
+            if not VersionControl.isBadVersion(m):
+                l = m+1
+            else:
+                h = m-1
+
+        return l
+
+
