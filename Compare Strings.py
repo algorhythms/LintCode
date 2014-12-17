505d517c68132dea33b6f898b25727c1db8da68f
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-12-17 10:51:53 +0800

    basics

---
+++ Compare Strings.py
@@ -0,0 +1,22 @@
+"""
+Compare two strings A and B, determine whether A contains all of the characters in B.
+"""
+__author__ = 'Danyang'
+class Solution:
+    def compareStrings(self, A, B):
+        """
+        :param A : A string includes Upper Case letters
+        :param B : A string includes Upper Case letters
+        :return :  if string A contains all of the characters in B return True else return False
+        """
+        cnt = [0 for _ in xrange(26)]
+        for c in A:
+            cnt[ord(c)-ord('A')] += 1
+        for c in B:
+            cnt[ord(c)-ord('A')] -= 1
+            if cnt[ord(c)-ord('A')]<0:
+                return False
+        return True
+
+if __name__=="__main__":
+    assert Solution().compareStrings("A", "")==True
---
+++ Hash Function.py
@@ -0,0 +1,24 @@
+"""
+In data structure Hash, hash function is used to convert a string(or any other type) into an integer smaller than hash
+size and bigger or equal to zero. The objective of designing a hash function is to "hash" the key as unreasonable as
+possible. A good hash function can avoid collision as less as possible. A widely used hash function algorithm is using
+a magic number 33
+"""
+__author__ = 'Danyang'
+class Solution:
+    def hashCode(self, key, HASH_SIZE):
+        """
+        :param key: A String you should hash
+        :param HASH_SIZE: An integer
+        :return an integer
+        """
+        w = 1
+        ret = 0
+        for i in xrange(len(key)-1, -1, -1):
+            ret = (ret+ord(key[i])*w)%HASH_SIZE
+            w = (w*33)%HASH_SIZE
+        return ret
+
+if __name__=="__main__":
+    assert Solution().hashCode("abcd", 100)==78
+
