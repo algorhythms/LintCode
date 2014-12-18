dc6b9aa422293b31dedcd21f0a32346c3e627e62
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-12-18 22:40:21 +0800

    forward backward dp

---
+++ Product of Array Exclude Itself.py
@@ -0,0 +1,30 @@
+"""
+Given an integers array A.
+
+Define B[i] = A[0] * ... * A[i-1] * A[i+1] * ... * A[n-1], calculate B without divide operation.
+
+Example
+For A=[1, 2, 3], B is [6, 3, 2]
+"""
+__author__ = 'Danyang'
+class Solution:
+    def productExcludeItself(self, A):
+        """
+
+        :param A: Given an integers array A
+        :return: An integer array B and B[i]= A[0] * ... * A[i-1] * A[i+1] * ... * A[n-1]
+        """
+        n = len(A)
+        if n==1:
+            return []
+
+        dp = [[1, 1] for _ in xrange(n)]
+        for i in xrange(1, n):
+            dp[i][0] = A[i-1]*dp[i-1][0]
+            dp[n-i-1][1] = A[n-i]*dp[n-i][1]
+
+        B = [dp[i][0]*dp[i][1] for i in xrange(n)]
+        return B
+
+if __name__=="__main__":
+    print Solution().productExcludeItself([1, 2, 3])
