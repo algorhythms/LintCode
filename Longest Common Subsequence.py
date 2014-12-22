99fd33a0b987553faf2ca3eee9ed74341bf20b00
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-12-22 22:20:32 +0800

    DP classics

---
+++ Longest Common Subsequence.py
@@ -0,0 +1,29 @@
+"""
+Given two strings, find the longest comment subsequence (LCS).
+
+Your code should return the length of LCS.
+"""
+__author__ = 'Danyang'
+class Solution:
+    def longestCommonSubsequence(self, A, B):
+        """
+
+        :param A: str
+        :param B: str
+        :return: The length of longest common subsequence of A and B.
+        """
+        m = len(A)
+        n = len(B)
+        f = [[0 for _ in xrange(n+1)] for _ in xrange(m+1)]
+
+        if m==0 or n==0:
+            return 0
+
+        for i in xrange(1, m+1):
+            for j in xrange(1, n+1):
+                if A[i-1]==B[j-1]:
+                    f[i][j] = f[i-1][j-1]+1
+                else:
+                    f[i][j] = max(f[i][j-1], f[i-1][j])
+
+        return f[-1][-1]
---
+++ Longest Common Substring.py
@@ -0,0 +1,36 @@
+"""
+Given two strings, find the longest common substring.
+
+Return the length of it.
+"""
+__author__ = 'Danyang'
+class Solution:
+    def longestCommonSubstring(self, A, B):
+        """
+        let f(i, j) represents the LCS END WITH A[i], B[j]
+        f(i, j) = f(i-1, j-1)+1 if A[i]==B[j]
+        f(i, j) = 0 otherwise
+
+        notice: return the maxa rather than f[-1]
+
+        :param A: str
+        :param B: str
+        :return: the length of the longest common substring.
+        """
+        m = len(A)
+        n = len(B)
+        f = [[0 for _ in xrange(n+1)] for _ in xrange(m+1)]
+
+        if m==0 or n==0:
+            return 0
+
+        maxa = -1
+        for i in xrange(1, m+1):
+            for j in xrange(1, n+1):
+                if A[i-1]==B[j-1]:
+                    f[i][j] = f[i-1][j-1]+1
+                else:
+                    f[i][j] = 0
+                maxa = max(maxa, f[i][j])
+
+        return maxa  # rather than f[-1][-1]
