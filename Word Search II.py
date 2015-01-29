0e17d1ffa268f6db95c58a21d6a7d1bd87dca7ca
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-01-29 12:18:43 +0800

    doc

--- Word Search II.py
+++ Word Search II.py
@@ -1,5 +1,6 @@
 """
-Given a matrix of lower alphabets and a dictionary. Find all words in the dictionary that can be found in the matrix. A word can start from any position in the matrix and go left/right/up/down to the adjacent position.
+Given a matrix of lower alphabets and a dictionary. Find all words in the dictionary that can be found in the matrix. A
+word can start from any position in the matrix and go left/right/up/down to the adjacent position.
 
 
 Example
