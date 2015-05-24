ad4611968927d4c648d2432c41c5b5edf3deb36b
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-05-24 23:19:51 +0800

    TLE

--- Coins in a Line III.py
+++ Coins in a Line III.py
@@ -56,9 +56,43 @@ class Solution:
 
         return F[0][0][n-1]>min(F[1][0][n-2], F[1][1][n-1])
 
-    def firstWillWin(self, values):
+    def firstWillWin_TLE(self, values):
         """
         optimize data structure
+
+        Time Limit Exceeded
+        :param values: a list of integers
+        :return: a boolean which equals to True if the first player will win
+        """
+        n = len(values)
+        if n == 1:
+            return True
+
+        SZ = 4
+        F = [[[0 for _ in xrange(SZ)] for _ in xrange(SZ)] for _ in xrange(2)]
+        s = [0 for _ in xrange(n+1)]
+        for i in xrange(1, n+1):
+            s[i] = s[i-1]+values[i-1]
+
+        for i in xrange(n-2, -1, -1):
+            for j in xrange(i+1, n):
+                for p in xrange(2):
+                    if j == i+1:
+                        a = values[i+1]
+                        b = values[i]
+                    else:
+                        a = F[1^p][(i+1)%SZ][j%SZ]
+                        b = F[1^p][i%SZ][(j-1)%SZ]
+
+                    F[p][i%SZ][j%SZ] = max(
+                        values[i]+s[j+1]-s[i+1]-a,
+                        values[j]+s[j]-s[i]-b
+                    )
+
+        return F[0][0][(n-1)%SZ] > min(F[1][0][(n-2)%SZ], F[1][1][(n-1)%SZ])
+
+    def firstWillWin(self, values):
+        """
 
         :param values: a list of integers
         :return: a boolean which equals to True if the first player will win
@@ -66,27 +100,11 @@ class Solution:
         n = len(values)
         if n == 1:
             return True
-        SZ = 4
-        F = [[[0 for _ in xrange(SZ)] for _ in xrange(SZ)] for _ in xrange(2)]
-        s = [0 for _ in xrange(n+1)]
-        for i in xrange(1, n+1):
-            s[i] = s[i-1]+values[i-1]
 
-        for i in xrange(n):
-            for p in xrange(2):
-                F[p][i%SZ][i%SZ] = values[i]
-
-        for i in xrange(n-2, -1, -1):
-            for j in xrange(i+1, n):
-                for p in xrange(2):
-                    F[p][i%SZ][j%SZ] = max(
-                        values[i]+s[j+1]-s[i+1]-F[1^p][(i+1)%SZ][j%SZ],
-                        values[j]+s[j]-s[i]-F[1^p][i%SZ][(j-1)%SZ]
-                    )
-
-        return F[0][0][(n-1)%SZ] > min(F[1][0][(n-2)%SZ], F[1][1][(n-1)%SZ])
+        # TODO 
 
 
 if __name__ == "__main__":
     print Solution().firstWillWin([3, 2, 2])
     print Solution().firstWillWin([1, 20, 4])
+    print Solution().firstWillWin([1, 2, 3, 4, 5, 6, 7, 8, 13, 11, 10, 9])
