ab3f13043cd0ce3213552ec26a24aa5066c0d147
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-11-07 10:28:54 -0800

    dp

--- Longest Palindromic Substring.py
+++ Longest Palindromic Substring.py
@@ -15,6 +15,26 @@ class Solution:
     def longestPalindrome(self, s):
         """
         O(n) Manacer's algorithm
+
+        O(n^2) dp
         :param s:
         :return:
         """
+        n = len(s)
+        pa = [[False for _ in xrange(n+1)] for _ in xrange(n)]
+        for i in xrange(n):
+            pa[i][i] = True
+            pa[i][i+1] = True
+
+        maxa = (0, 1)
+        for i in xrange(n-2, -1, -1):
+            for j in xrange(i+2, n+1):
+                pa[i][j] = pa[i+1][j-1] and s[i] == s[j-1]
+                if pa[i][j] and j-i > maxa[1]-maxa[0]:
+                    maxa = (i, j)
+
+        return s[maxa[0]:maxa[1]]
+
+
+if __name__ == "__main__":
+    assert Solution().longestPalindrome("abcdzdcab") == "cdzdc"
